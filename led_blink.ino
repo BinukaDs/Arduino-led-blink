2cc4eb1dd497bf0ccd4e2b929f71491e90c62181
BinukaDasunpriya <77571607+BinukaDasunpriya@users.noreply.github.com>
2021-01-20 16:45:37 +0530

    Update led_blink.ino

--- led_blink.ino
+++ led_blink.ino
@@ -1,9 +1,41 @@
+/*
+
+* ***************************  
+  written : Binuka Dasunpriya
+  date : 10th January 2021
+  updated : 20th January 2021
+  updated By : Binuka Dasunpriya
+  Email : binukadasunpriya@gmail.com
+  GitHub : https://github.com/BinukaDasunpriya
+
+* ***************************  
+
+* ***************************
+we need : a led
+        : a arduino Uno/nano/mega board
+        : 2 jumpers
+        : a breadboard 
+        
+        
+Led pins : led + = arduino  5 pin
+         : led - = adruino Gnd pin      
+* ***************************
+
+ */
+
+
+// initiallize led
+int led 5
 void setup() {
- pinMode(5, OUTPUT);
+ // initiallize led as a output
+ pinMode(led, OUTPUT);
 } 
 void loop() {
- digitalWrite(5, HIGH);
+ //led on
+ digitalWrite(led, HIGH);
+ //delay
  delay(1000);
- digitalWrite(5, LOW);
+ //led off
+ digitalWrite(led, LOW);
 
 } 
